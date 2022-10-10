6310a53865a6fe34c9eeef356fe2671fb5e8fc6b
sergey <glotovs198@gmail.com>
2022-10-10 09:42:34 +0300

    first commit

---
+++ src/pages/ConversationChannelPage.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const ConversationChannelPage: FC = () => {
+	return <div>Channel Page</div>;
+};
+
+export default ConversationChannelPage;
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import RegisterForm from '../components/forms/RegisterForm';
+
+const AuthenticationPage: FC = () => {
+	return (
+		<div>
+			<RegisterForm />
+		</div>
+	);
+};
+
+export default AuthenticationPage;
